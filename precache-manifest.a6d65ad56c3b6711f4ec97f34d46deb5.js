6ac69f8aa00a931e401b697280c9b0758f43d78a
riojuliian <riojuliian@gmail.com>
2020-10-06 20:33:20 +0700

    Updates

---
+++ precache-manifest.a6d65ad56c3b6711f4ec97f34d46deb5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8c6fd14427db49ceffa44f81033772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cf8fca6c38a5e3694b6",
+    "url": "/static/css/3.9998556b.chunk.css"
+  },
+  {
+    "revision": "b336832e17d3e71ebeef",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "1b9e5879daed81516321",
+    "url": "/static/css/main.639ee62f.chunk.css"
+  },
+  {
+    "revision": "5f207b0ddafb58202283",
+    "url": "/static/js/2.43a924bf.chunk.js"
+  },
+  {
+    "revision": "5cf8fca6c38a5e3694b6",
+    "url": "/static/js/3.e7ff7e8b.chunk.js"
+  },
+  {
+    "revision": "b336832e17d3e71ebeef",
+    "url": "/static/js/4.7b0ecb2e.chunk.js"
+  },
+  {
+    "revision": "1b9e5879daed81516321",
+    "url": "/static/js/main.5c13ac9d.chunk.js"
+  },
+  {
+    "revision": "505e36390423a4f567a7",
+    "url": "/static/js/runtime-main.596cbad2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ec49a6158efb9eaebd6110183e794a3a",
+    "url": "/static/media/apLogo-02.ec49a615.jpg"
+  },
+  {
+    "revision": "6bc56850eab351752256729a44d20f37",
+    "url": "/static/media/certification-02.6bc56850.jpg"
+  },
+  {
+    "revision": "2fa54778ae7c1b415bd1141094c90245",
+    "url": "/static/media/certification-03.2fa54778.jpg"
+  },
+  {
+    "revision": "157a15850e0285d80a1700648cf0a5b8",
+    "url": "/static/media/certification-04.157a1585.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "13c0012812d46645e1eff4409f210a50",
+    "url": "/static/media/jajal1-01.13c00128.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3f699735fb44f784ba7166bf16a1f2d3",
+    "url": "/static/media/project-05.3f699735.jpg"
+  },
+  {
+    "revision": "176e5d2df7986f05fcdfc751572040ad",
+    "url": "/static/media/project-06.176e5d2d.jpg"
+  },
+  {
+    "revision": "696ab842a377dc427df7f1fca4681aee",
+    "url": "/static/media/project-07.696ab842.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a84c692bf72fb13adbe63c9b5f9960b6",
+    "url": "/static/media/undipLogoTr-04.a84c692b.png"
+  }
+]);
